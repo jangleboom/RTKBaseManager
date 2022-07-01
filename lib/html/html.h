e96dd326a7cdecb17c5f6b4454479bc16da68f1e
Mr. Markuese <mrmarkuese@cookbook.lan>
2022-07-01 19:20:43 +0200

    second commit

---
+++ lib/html/html.h
@@ -0,0 +1,169 @@
+#ifndef HTML_H
+#define HTML_H
+
+const char INDEX_HTML[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta content="text/html" ; charset="UTF-8" ; http-equiv="content-type">
+    <meta name="viewport" content="width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0">
+    <title>RTK base setup</title>
+    <style>
+        body {
+            background-color: #4180C8;
+            font-family: Arial, Helvetica, sans-serif;
+            Color: #ffffff;
+            text-align: center;
+            border: 1em;
+        }
+
+        .center {
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .button {
+            background-color: #F0A03C;
+            border: none;
+            color: white;
+            padding: 13px 18px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 13px;
+        }
+
+        .text_field {
+            border: none;
+            color: black;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 13px;
+        }
+
+        ::placeholder {
+            color: navy;
+            opacity: 1;
+            transition: opacity 1s;
+        }
+
+        :focus::placeholder {
+            opacity: 0
+        }
+    </style>
+</head>
+<script>
+    function loadRadioState() {
+        if (document.getElementById("radio_state").value == "survey_enabled") {
+            (document.getElementById("survey_enabled").checked = true);
+        } else {
+            (document.getElementById("coords_enabled").checked = true);
+        }
+        enableChoosenMethod();
+    }
+
+    function enableChoosenMethod() {
+        if (document.getElementById("survey_enabled").checked == false) {
+            document.getElementById("survey_accuracy").disabled = true;
+            document.getElementById("latitude").disabled = false;
+            document.getElementById("longitude").disabled = false;
+            document.getElementById("height").disabled = false;
+        } else {
+            document.getElementById("survey_accuracy").disabled = false;
+            document.getElementById("latitude").disabled = true;
+            document.getElementById("longitude").disabled = true;
+            document.getElementById("height").disabled = true;
+        }
+    }
+</script>
+
+<body onload="loadRadioState()">
+    <form id="Form1" onsubmit="return confirm('Please restart the ESP32 board by pressing the Reboot button after saving your data!');" action='actionUpdateData' method='post' target="hidden-form"></form>
+    <form id="Form2" onsubmit="return confirm('Are you sure? All saved SPIFFS files will be deleted (Wifi and RTK config)');" action='actionWipeData' method='post' target="hidden-form"></form>
+    <form id="Form3" onsubmit="return confirm('Connection will be lost during reboot, please refresh this page after reconnecting!');" action='actionReboot' method='post' target="hidden-form"></form>
+    <p>
+        <table class=center>
+            <tr>
+                <td colspan=2>
+                    <h2>RTK Base Station</h2>
+                </td>
+                <td colspan=2></td>
+            </tr>
+            <tr>
+                <td colspan=2>
+                    <h3>WiFi credentials</h3>
+                </td>
+                <td colspan=2></td>
+            </tr>
+            <tr>
+                <td style="text-align:left;">SSID:</td>
+                <td>
+                    <input class="text_field" form="Form1" type="text" maxlength="30" name="ssid" placeholder=%ssid% style="text-align:center;">
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align:left;">Password:</td>
+                <td>
+                    <input class="text_field" form="Form1" type="text" maxlength="30" name="password" placeholder=%password% style="text-align:center;">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td style="text-align:right;"> </td>
+            </tr>
+            <tr>
+                <td colspan=2>
+                    <h3>RTK configuration</h3>
+                </td>
+                <td colspan=2></td>
+            </tr>
+            <tr>
+                <td colspan=2>
+                    Choose location method:
+                </td>
+                <td colspan=2></td>
+            </tr>
+            <tr>
+                <td colspan=2> <input form="Form1" type="hidden" id="radio_state" value=%location_method% placeholder=%location_method%</td>
+            </tr>
+            <tr>
+                <td style="text-align:right;"> <input form="Form1" type="radio" id="survey_enabled" value="survey_enabled" name="location_method" onclick="enableChoosenMethod()"></td>
+                <td style="text-align:left;" title="For an accuracy of 0.06 m this can take up to 24 hours."> <label for="survey_enabled"> Run a long survey </label></td>
+            </tr>
+            <tr>
+                <td style="text-align:right;"> <input form="Form1" type="radio" id="coords_enabled" value="coords_enabled" name="location_method" onclick="enableChoosenMethod()"></td>
+                <td style="text-align:left;" title="Seven decimal places are required."> <label for="coords_enabled"> High precision coordinates </label></td>
+            </tr>
+            <td colspan=2></td>
+            <tr>
+                <td style="text-align:left;"> Accuracy in m: </td>
+                <td><input title="The survey is carried out until the desired accuracy is achieved. After that, the location coordinates are stored in SPIFFS. 0.06 m is a useful value." class="text_field" form="Form1" type="text" maxlength="30" id="survey_accuracy" name="survey_accuracy" placeholder=%survey_accuracy%></td>
+            </tr>
+            <tr>
+                <td style="text-align:left;"> Latitude: </td>
+                <td><input class="text_field" form="Form1" type="text" maxlength="30" id="latitude" name="latitude" placeholder=%latitude%></td>
+            </tr>
+            <tr>
+                <td style="text-align:left;"> Longitude: </td>
+                <td><input class="text_field" form="Form1" type="text" maxlength="30" id="longitude" name="longitude" placeholder=%longitude%></td>
+            </tr>
+            <tr>
+                <td style="text-align:left;"> Height in m: </td>
+                <td><input title="Height over sea-level of the antenna is required (float)." class="text_field" form="Form1" type="text" maxlength="30" id="height" name="height" placeholder=%height%></td>
+            </tr>
+        </table>
+    </p>
+    <br>
+    <div>
+        <input type="submit" form="Form1" formaction="/actionUpdateData" class="button" value="Save" id="save_button" name="save_button" />
+        <input type="submit" form="Form3" value="Reboot" class="button" />
+        <input type="reset" form="Form1" value="Cancel" class="button" />
+        <input type="submit" form="Form2" formaction="/actionWipeData" class="button" value="Wipe" id="wipe_button" name="wipe_button" />
+    </div>
+</body>
+
+</html>
+)rawliteral";
+#endif /* HTML_H */
---
+++ src/main.cpp
@@ -0,0 +1,383 @@
+
+#include <Arduino.h>
+#include <ESPmDNS.h>
+
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <AsyncTCP.h>
+  #include <SPIFFS.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESPAsyncTCP.h>
+  #include <Hash.h>
+  #include <FS.h>
+#endif
+
+#include <ESPAsyncWebServer.h>
+#include "html.h"
+
+/** TODO: 
+          - upload html and css to SPIFFS 
+          - save success info after submit
+*/
+
+AsyncWebServer server(80);
+
+// #define TESTING
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "Loewenzahn";
+const char* password = "vollverschluesselt123!";
+const char* DEVICE_NAME = "rtkbase";
+// WiFi credentials for AP mode
+const char *ssidAP = "Rtkbase";
+const char *passwordAP = "12345678";
+
+// WiFi and RTKBase manager
+const char* PARAM_WIFI_SSID = "ssid";
+const char* PARAM_WIFI_PASSWORD = "password";
+const char* PARAM_RTK_LOCATION_METHOD = "location_method";
+const char* PARAM_RTK_SURVEY_ENABLED = "survey_enabled";
+const char* PARAM_RTK_COORDS_ENABLED = "coords_enabled";
+const char* PARAM_RTK_LOCATION_SURVEY_ACCURACY = "survey_accuracy";
+const char* PARAM_RTK_LOCATION_LONGITUDE = "longitude";
+const char* PARAM_RTK_LOCATION_LATITUDE = "latitude";
+const char* PARAM_RTK_LOCATION_HEIGHT = "height";
+const char PATH_WIFI_SSID[] = "/ssid.txt";
+const char PATH_WIFI_PASSWORD[] = "/password.txt";
+const char PATH_RTK_LOCATION_METHOD[] = "/location_method.txt";
+// const char PATH_RTK_SURVEY_ENABLED[] = "/survey_enabled.txt";
+// const char PATH_RTK_COORDS_ENABLED[] = "/coords_enabled.txt";
+const char PATH_RTK_LOCATION_SURVEY_ACCURACY[] = "/survey_accuracy.txt";
+const char PATH_RTK_LOCATION_LONGITUDE[] = "/longitude.txt";
+const char PATH_RTK_LOCATION_LATITUDE[] = "/latitude.txt";
+const char PATH_RTK_LOCATION_HEIGHT[] = "/height.txt";
+
+const uint8_t MAX_SSIDS = 10;
+String seenSSIDs[MAX_SSIDS];
+// WiFi
+int scanWiFiAPs(void);
+int setupWiFiAP(const String& ssid);
+bool knownNetworkAvailable(void);
+// Web server
+String processor(const String& var);
+void notFound(AsyncWebServerRequest *request);
+void actionReboot(AsyncWebServerRequest *request);
+void actionWipeData(AsyncWebServerRequest *request);
+void actionUpdateData(AsyncWebServerRequest *request);
+// SPIFFS
+void writeFile(fs::FS &fs, const char* path, const char* message);
+String readFile(fs::FS &fs, const char* path);
+void listAllFiles(void);
+void wipeAllSpiffsFiles(void);
+
+
+void setup() {
+  Serial.begin(115200);
+  // Initialize SPIFFS
+  #ifdef ESP32
+    if (!SPIFFS.begin(true)) {
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
+    }
+  #else
+    if (!SPIFFS.begin()) {
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
+    }
+  #endif
+  
+  WiFi.setHostname(DEVICE_NAME);
+  if (!knownNetworkAvailable()) {
+    int foundAPs = scanWiFiAPs();
+    for (int i=0; i<foundAPs; i++) {
+      Serial.printf("%d %s\n", i+1, seenSSIDs[i].c_str());
+    }
+    delay(1000);
+    Serial.print("Setting soft-AP ... ");
+    WiFi.mode(WIFI_AP);
+    // WiFi.softAP(ssidAP, passwordAP);
+    Serial.println(WiFi.softAP(ssidAP, passwordAP) ? "Ready" : "Failed!");
+    Serial.print("Access point started: ");
+    Serial.println(ssidAP);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.softAPIP());
+    delay(1000);
+ } else {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    // TODO:  - count reboots and stop after 3 times (save in SPIFFS)
+    //        - display state
+    Serial.println("WiFi Failed! Reboot in 10 s as AP!");
+    delay(10000);
+    ESP.restart();
+  }
+  Serial.println();
+
+  if (!MDNS.begin(DEVICE_NAME)) {
+      Serial.println("Error starting mDNS, use local IP instead!");
+  } else {
+    Serial.printf("Starting mDNS, find me under <http://www.%s.local>\n", DEVICE_NAME);
+  }
+
+  Serial.print("Wifi client started: ");
+  Serial.println(WiFi.getHostname());
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  }
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", INDEX_HTML, processor);
+  });
+
+  server.on("/actionUpdateData", HTTP_POST, actionUpdateData);
+  server.on("/actionWipeData", HTTP_POST, actionWipeData);
+  server.on("/actionReboot", HTTP_POST, actionReboot);
+
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void loop() {
+  
+}
+
+/********************************************************************************
+*                             WiFi
+* ******************************************************************************/
+bool knownNetworkAvailable() {
+  String savedSSID = readFile(SPIFFS, PATH_WIFI_SSID);
+  int nNetworks = scanWiFiAPs();
+  for (int i=0; i<nNetworks; i++) {
+    if (savedSSID.equals(seenSSIDs[i])) {
+      Serial.printf("Known network with SSID %s found, connecting...", seenSSIDs[i]);
+      return true;
+    }
+  }
+  Serial.printf("Known network with SSID %s not found, starting AP to enter new credentials\n", savedSSID);
+  return false;
+}
+
+int scanWiFiAPs() {
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    // Delete old entries
+    for (int i = 0; i < MAX_SSIDS; i++) {
+        seenSSIDs[i] = "";
+    }
+
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+  
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" network(s) found");
+        for (int i = 0; i < n; i++) {
+            // Print SSID and RSSI for each network found
+            seenSSIDs[i] = String(WiFi.SSID(i));//+ " (" + String(WiFi.RSSI(i)) + ")";
+            Serial.println(seenSSIDs[i]);
+            delay(10);
+            if (n == MAX_SSIDS) break;
+        }
+    }
+    Serial.println("");
+    // Wait a bit before scanning again
+    delay(3000);  
+    return n;
+}
+
+
+/********************************************************************************
+*                             Web server
+* ******************************************************************************/
+void actionUpdateData(AsyncWebServerRequest *request) {
+  Serial.println("ACTION 1!");
+
+  int params = request->params();
+  for (int i = 0; i < params; i++) {
+    AsyncWebParameter* p = request->getParam(i);
+    Serial.printf("%d. POST[%s]: %s\n", i+1, p->name().c_str(), p->value().c_str());
+
+    if (strcmp(p->name().c_str(), PARAM_WIFI_SSID) == 0) {
+      if (p->value().length() > 0) {
+        writeFile(SPIFFS, PATH_WIFI_SSID, p->value().c_str());
+     } 
+    }
+
+    if (strcmp(p->name().c_str(), PARAM_WIFI_PASSWORD) == 0) {
+      if (p->value().length() > 0) {
+        writeFile(SPIFFS, PATH_WIFI_PASSWORD, p->value().c_str());
+     } 
+    }
+
+    if (strcmp(p->name().c_str(), PARAM_RTK_LOCATION_METHOD) == 0) {
+      if (p->value().length() > 0) {
+        writeFile(SPIFFS, PATH_RTK_LOCATION_METHOD, p->value().c_str());
+     } 
+    }
+
+    if (strcmp(p->name().c_str(), PARAM_RTK_LOCATION_SURVEY_ACCURACY) == 0) {
+      if (p->value().length() > 0) {
+        writeFile(SPIFFS, PATH_RTK_LOCATION_SURVEY_ACCURACY, p->value().c_str());
+     } 
+    }
+
+    if (strcmp(p->name().c_str(), PARAM_RTK_LOCATION_LATITUDE) == 0) {
+      if (p->value().length() > 0) {
+        writeFile(SPIFFS, PATH_RTK_LOCATION_LATITUDE, p->value().c_str());
+     } 
+    }
+
+    if (strcmp(p->name().c_str(), PARAM_RTK_LOCATION_LONGITUDE) == 0) {
+      if (p->value().length() > 0) {
+        writeFile(SPIFFS, PATH_RTK_LOCATION_LONGITUDE, p->value().c_str());
+     } 
+    }
+
+    if (strcmp(p->name().c_str(), PARAM_RTK_LOCATION_HEIGHT) == 0) {
+      if (p->value().length() > 0) {
+        writeFile(SPIFFS, PATH_RTK_LOCATION_HEIGHT, p->value().c_str());
+     } 
+    }
+
+  }
+  Serial.println(F("Data saved to SPIFFS!"));
+  request->send_P(200, "text/html", INDEX_HTML, processor);
+}
+
+void actionWipeData(AsyncWebServerRequest *request) {
+  Serial.println("ACTION 2!");
+  int params = request->params();
+  for (int i = 0; i < params; i++) {
+    AsyncWebParameter* p = request->getParam(i);
+    Serial.printf("%d. POST[%s]: %s\n", i+1, p->name().c_str(), p->value().c_str());
+    if (strcmp(p->name().c_str(), "wipe_button") == 0) {
+      if (p->value().length() > 0) {
+        Serial.printf("wipe command received: %s",p->value().c_str());
+        // WIPE OUT ALL PATHS
+        wipeAllSpiffsFiles();
+     } 
+    }
+  }
+  Serial.println(F("Data in SPIFFS was wiped out!"));
+  request->send_P(200, "text/html", INDEX_HTML, processor);
+}
+
+void actionReboot(AsyncWebServerRequest *request) {
+  Serial.println("ACTION 3!");
+  request->send_P(200, "text/html", INDEX_HTML, processor);
+  delay(3000);
+  ESP.restart();
+}
+
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+// Replaces placeholder with stored values
+String processor(const String& var) {
+  if (var == PARAM_WIFI_SSID) {
+    String savedSSID = readFile(SPIFFS, PATH_WIFI_SSID);
+    return (savedSSID.isEmpty() ? String(PARAM_WIFI_SSID) : savedSSID);
+  }
+  else if (var == PARAM_WIFI_PASSWORD) {
+    String savedPassword = readFile(SPIFFS, PATH_WIFI_PASSWORD);
+    return (savedPassword.isEmpty() ? String(PARAM_WIFI_PASSWORD) : "*******");
+  }
+  else if (var == PARAM_RTK_LOCATION_METHOD) {
+    String locationMethod = readFile(SPIFFS, PATH_RTK_LOCATION_METHOD);
+    return (locationMethod.isEmpty() ? String(PARAM_RTK_SURVEY_ENABLED) : locationMethod);
+  }
+  else if (var == PARAM_RTK_LOCATION_SURVEY_ACCURACY) {
+    String surveyAccuracy = readFile(SPIFFS, PATH_RTK_LOCATION_SURVEY_ACCURACY);
+    return (surveyAccuracy.isEmpty() ? String(PARAM_RTK_LOCATION_SURVEY_ACCURACY) : surveyAccuracy);
+  }
+  else if (var == PARAM_RTK_LOCATION_LATITUDE) {
+    String savedLatitude = readFile(SPIFFS, PATH_RTK_LOCATION_LATITUDE);
+    return (savedLatitude.isEmpty() ? String(PARAM_RTK_LOCATION_LATITUDE) : savedLatitude);
+  }
+  else if (var == PARAM_RTK_LOCATION_LONGITUDE) {
+    String savedLongitude = readFile(SPIFFS, PATH_RTK_LOCATION_LONGITUDE);
+    return (savedLongitude.isEmpty() ? String(PARAM_RTK_LOCATION_LONGITUDE) : savedLongitude);
+  }
+  else if (var == PARAM_RTK_LOCATION_HEIGHT) {
+    String savedHeight = readFile(SPIFFS, PATH_RTK_LOCATION_HEIGHT);
+    return (savedHeight.isEmpty() ? String(PARAM_RTK_LOCATION_HEIGHT) : savedHeight);
+  }
+  return String();
+}
+
+/********************************************************************************
+*                             Data storage (SPIFFS)
+* ******************************************************************************/
+void writeFile(fs::FS &fs, const char* path, const char* message) {
+  Serial.printf("Writing file: %s\r\n", path);
+
+  File file = fs.open(path, "w");
+  if (!file) {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("- file written");
+  } else {
+    Serial.println("- write failed");
+  }
+  file.close();
+}
+
+String readFile(fs::FS &fs, const char* path) {
+  Serial.printf("Reading file: %s\r\n", path);
+  File file = fs.open(path, "r");
+
+  if (!file || file.isDirectory()) {
+    Serial.println("- empty file or failed to open file");
+    return String();
+  }
+  Serial.println("- read from file:");
+  String fileContent;
+
+  while (file.available()) {
+    fileContent += String((char)file.read());
+  }
+  file.close();
+  Serial.println(fileContent);
+
+  return fileContent;
+}
+
+void listAllFiles() {
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+ 
+  while (file) {
+      Serial.print("FILE: ");
+      Serial.println(file.name());
+      file = root.openNextFile();
+  }
+  file.close();
+  root.close();
+}
+
+void wipeAllSpiffsFiles() {
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+
+  Serial.println(F("Wiping: "));
+
+  while (file) {
+    Serial.print("FILE: ");
+    Serial.println(file.name());
+    SPIFFS.remove(file.name());
+    file = root.openNextFile();
+  }
+  //  file.close();
+  listAllFiles();
+
+}
