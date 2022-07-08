ac3a891a89a8bbe52b347af57b736155743288ba
Mr. Markuese <mrmarkuese@MacBook-Pro.lan>
2022-07-08 15:54:55 +0200

    separated setup func for wifi

--- lib/RTKManager/RTKBaseManager.h
+++ lib/RTKManager/RTKBaseManager.h
@@ -11,7 +11,9 @@
  *          - a check of the number of decimal places in the input of the geo-coordinates 
  *            with regard to a suitable level of accuracy
  *          - upload html and (separated css and js) to SPIFFS 
- * @note    
+ * 
+ * @note    FYI: A good tutorial about how to transfer input data from a from and save them to SPIFFS
+ *          https://medium.com/@adihendro/html-form-data-input-c942ba23224
  */
 
 #ifndef RTK_MANAGER_H
